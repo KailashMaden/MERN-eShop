581c9de6a7bdc8227f03adf2e696248c72bd7af6
kailashmaden <nedamkailash12@gmail.com>
2020-11-13 20:23:08 +0545

    cart functionality completed

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import axios from 'axios';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const {data} = await axios.get(`/api/products/${id}`)
+  const { data } = await axios.get(`/api/products/${id}`);
 
   dispatch({
     type: CART_ADD_ITEM,
@@ -12,8 +12,17 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       image: data.image,
       price: data.price,
       countInStock: data.countInStock,
-      qty
-    }
-  })
-  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-}
+      qty,
+    },
+  });
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
