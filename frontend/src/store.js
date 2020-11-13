1edd956577fe15a810bc97ecf6bd890c7142d665
kailashmaden <nedamkailash12@gmail.com>
2020-11-13 12:52:17 +0545

    productDetailsReducer and action created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 // connect our reducers and middleware
+// 3. after creating reducers add in storejs i.e here to work redux
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/productReducers';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
