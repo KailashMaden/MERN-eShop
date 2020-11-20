220be0ce03f38b7a2bc56c49dc70fade4e9b3c19
kailashmaden <nedamkailash12@gmail.com>
2020-11-20 22:17:00 +0545

    Order Screen created

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -42,7 +42,6 @@ export const orderDetailsReducer = (
     case ORDER_DETAILS_SUCCESS:
       return {
         loading: false,
-        success: true,
         order: action.payload,
       };
     case ORDER_DETAILS_FAIL:
