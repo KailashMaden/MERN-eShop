a0ba18cabcb80e1eeb00119958145aad28435567
kailashmaden <nedamkailash12@gmail.com>
2020-11-24 19:21:57 +0545

    admin delete products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,7 +10,6 @@ const getProducts = asyncHandler(async (req, res) => {
   res.json(products);
 });
 
-
 //@desc Fetch single product
 //@route GET /api/products/:id
 //@access Public
@@ -25,4 +24,19 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @desc Delete a product
+// @route DELETE /api/products/:id
+// @access Private/admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export { getProducts, getProductById, deleteProduct };
